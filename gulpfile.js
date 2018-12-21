d27b4c9f62f18baca111234dd83984d98e7ef52d
Kevin Nguyen <minhkhoi.nguyen@solislab.com>
2018-12-21 22:04:05 +0700

    Add package gulp-standard gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,50 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var plumber = require('gulp-plumber');
-var uglifiy = require('gulp-uglify');
-var autoprefixer = require('gulp-autoprefixer');
-var browserify = require('gulp-browserify');
-var livereload = require('gulp-livereload');
-
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var sourcemaps = require('gulp-sourcemaps')
+var plumber = require('gulp-plumber')
+var uglifiy = require('gulp-uglify')
+var autoprefixer = require('gulp-autoprefixer')
+var browserify = require('gulp-browserify')
+var livereload = require('gulp-livereload')
+var standard = require('gulp-standard')
 var cssDest = 'assets/css/',
-	jsDest = 'assets/js/';
+  jsDest = 'assets/js/'
 
 var autoprefixerOptions = {
-	browsers: ['last 3 versions', '> 5%', 'Firefox ESR']
-};
+  browsers: ['last 3 versions', '> 5%', 'Firefox ESR']
+}
 
-gulp.task('styles', function(){
-	gulp.src('src/scss/main.scss')
-		.pipe(sourcemaps.init())
-		.pipe(plumber())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(sourcemaps.write())
-		.pipe(plumber.stop())
-		.pipe(autoprefixer(autoprefixerOptions))
-		.pipe(gulp.dest(cssDest))
-		.pipe(livereload());
-});
+gulp.task('styles', function () {
+  gulp.src('src/scss/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(plumber.stop())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(cssDest))
+    .pipe(livereload())
+})
 
-gulp.task('scripts', function() {
-	gulp.src('src/js/*.js')
-		.pipe(browserify())
-		.pipe(uglifiy())
-		.pipe(gulp.dest(jsDest));
-});
+gulp.task('scripts', function () {
+  gulp.src('src/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true,
+      quiet: true
+    }))
+    .pipe(plumber())
+    .pipe(browserify())
+    .pipe(uglifiy())
+    .pipe(gulp.dest(jsDest))
+})
 
-gulp.task('watch',function() {
-	livereload.listen();
-	gulp.watch('src/scss/**/*.scss',['styles']);
-	gulp.watch('src/js/**/*.js', ['scripts']);
-});
+gulp.task('watch', function () {
+  livereload.listen()
+  gulp.watch('src/scss/**/*.scss', ['styles'])
+  gulp.watch('src/js/**/*.js', ['scripts'])
+})
 
-gulp.task('build', ['styles', 'scripts']);
-gulp.task('default', ['styles', 'scripts', 'watch']);
+gulp.task('build', ['styles', 'scripts'])
+gulp.task('default', ['styles', 'scripts', 'watch'])
--- src/js/customizer.js
+++ src/js/customizer.js
@@ -1,18 +1,9 @@
+var wp = global.wp
+
 document.addEventListener('DOMContentLoaded', function () {
-  const wp = global.wp
   wp.customize('footer_copyright_text_setting', function (value) {
     value.bind(function (newval) {
       document.querySelector('.js-footer-copyright-text').innerHTML = newval
-    })
-  })
-  wp.customize('hero_homepage_headline_setting', function (value) {
-    value.bind(function (newval) {
-      document.querySelector('.js-hero-headline').innerHTML = newval
-    })
-  })
-  wp.customize('hero_homepage_description_setting', function (value) {
-    value.bind(function (newval) {
-      document.querySelector('.js-hero-description').innerHTML = newval
     })
   })
 })
