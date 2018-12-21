45f340e47e40f20041228d92b8158ea8fd817a39
Kevin Nguyen <minhkhoi.nguyen@solislab.com>
2018-12-21 21:42:13 +0700

    Update customizer.js based on standardJS

--- src/js/customizer.js
+++ src/js/customizer.js
@@ -1,8 +1,18 @@
-( function( $ ) {
-	console.log('cutomizer.js was included!');
-	wp.customize( 'footer_copyright_text_setting', function( value ) {
-		value.bind( function( newval ) {
-			$( '.js-footer-copyright-text' ).html( newval );
-		} );
-	} );
-} )( jQuery );
+document.addEventListener('DOMContentLoaded', function () {
+  const wp = global.wp
+  wp.customize('footer_copyright_text_setting', function (value) {
+    value.bind(function (newval) {
+      document.querySelector('.js-footer-copyright-text').innerHTML = newval
+    })
+  })
+  wp.customize('hero_homepage_headline_setting', function (value) {
+    value.bind(function (newval) {
+      document.querySelector('.js-hero-headline').innerHTML = newval
+    })
+  })
+  wp.customize('hero_homepage_description_setting', function (value) {
+    value.bind(function (newval) {
+      document.querySelector('.js-hero-description').innerHTML = newval
+    })
+  })
+})
